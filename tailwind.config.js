a315dffe0ed818128cba95db11d2b9056f795fe0
Uday Kiran <umadineni@convoso.com>
2026-03-24 15:37:27 +0530

    Implementation of child routes for Active Defense Component

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './projects/con-joc-app/src/**/*.{html,ts,scss}',
+    './projects/theme/src/lib/styles/**/*.{scss,css}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
