6526ffae423236409f781a313f8201bbb48877f7
Uday Kiran <umadineni@convoso.com>
2026-04-07 12:40:06 +0530

    Implementation of child routes for Purpose  Component

--- projects/con-joc-app/src/app/data-access/tools/purpose/mocks/purpose.mock.ts
+++ projects/con-joc-app/src/app/data-access/tools/purpose/mocks/purpose.mock.ts
@@ -5,7 +5,7 @@ export const purpose:PurposeConfig[]=[
     "sl": 1,
     "purpose": "COMMNOTM",
     "description": "Commercial Non-Telemarketing",
-    "active": true,
+    "active": 'yes',
     "created_at": "2025-07-23 12:21:13",
     "updated_at": "2025-07-23 12:21:13"
   },
@@ -13,7 +13,7 @@ export const purpose:PurposeConfig[]=[
     "sl": 2,
     "purpose": "NONCOMM",
     "description": "Non-Commercial",
-    "active": true,
+    "active": 'yes',
     "created_at": "2025-07-23 12:20:54",
     "updated_at": "2025-07-23 12:20:54"
   },
@@ -21,7 +21,7 @@ export const purpose:PurposeConfig[]=[
     "sl": 3,
     "purpose": "NOTIFY",
     "description": "Notification",
-    "active": true,
+    "active": 'yes',
     "created_at": "2025-07-23 12:20:38",
     "updated_at": "2025-07-23 12:20:38"
   },
@@ -29,7 +29,7 @@ export const purpose:PurposeConfig[]=[
     "sl": 4,
     "purpose": "COLLECT",
     "description": "Collections",
-    "active": true,
+    "active": 'yes',
     "created_at": "2025-07-23 12:20:09",
     "updated_at": "2025-12-24 06:03:38"
   },
@@ -37,7 +37,7 @@ export const purpose:PurposeConfig[]=[
     "sl": 5,
     "purpose": "HIPAA",
     "description": "HIPAA Related",
-    "active": true,
+    "active": 'yes',
     "created_at": "2025-07-23 12:19:52",
     "updated_at": "2026-01-07 20:39:11"
   }
